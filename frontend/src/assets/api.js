5446e495e7d998f5f3ebdebbfa21122a23c747f1
Ranjith-Reddy09 <ranjithkatukuri09@gmail.com>
2025-05-23 17:15:31 +0530

    Add files via upload

---
+++ frontend/src/assets/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'https://leucine-tech-internship-0fwc.onrender.com/api' });
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem('token');
+  if (token) req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
+
+export default API;
